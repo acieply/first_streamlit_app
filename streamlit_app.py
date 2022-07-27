c1246d4b2aeca898775af892e092a359b72138ac
acieply <arturcieplyy@gmail.com>
2022-07-27 11:56:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
